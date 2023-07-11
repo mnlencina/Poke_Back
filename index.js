3b4a57795dacd60f9c7245a4648fc18575281f9e
mnlencina <mnlencina@gmail.com>
2023-07-11 12:13:27 -0300

    deploy 3

--- index.js
+++ index.js
@@ -17,12 +17,14 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+require('dotenv').config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const PORT = process.env.PORT || 3001
 
 // Syncing all the models at once.
 conn.sync({ alter: false }).then(() => {
-  server.listen(3001, () => {
+  server.listen(PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
